d97f2338117651ba0bbe547de1f03c955d59202d
eulernasc <129453478+eulernasc@users.noreply.github.com>
2026-08-20 10:42:26 -0300

    Add files via upload

---
+++ ASSETS.md
@@ -0,0 +1,7 @@
+# Assets visuais (CC0 / domínio público)
+
+- Gorilla (#2), OpenClipart, public domain: https://openclipart.org/detail/327115/gorilla-2
+- Saxophone, TheresaKnott / OpenClipart, CC0: https://openclipart.org/detail/312/saxophone
+- Banana, nicubunu / OpenClipart, CC0: https://openclipart.org/detail/12985/banana
+
+Os arquivos foram incorporados ao projeto e podem ser modificados livremente.
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'bruno-gorila-sax-v3';
-const FILES = ['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./assets/icon.svg','./assets/sax-riff.wav'];
+const CACHE = 'bruno-gorila-sax-v4';
+const FILES = ['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./assets/icon.svg','./assets/sax-riff.wav','./assets/bruno-gorilla-base.png','./assets/saxophone-cc0.png','./assets/banana-cc0.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', e => {
