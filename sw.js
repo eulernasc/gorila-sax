8dc85dd64edaba160309d702b702620090cca32b
eulernasc <129453478+eulernasc@users.noreply.github.com>
2026-08-20 11:54:07 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bruno-gorila-v5-rig';
+const CACHE='bruno-gorila-v6-layout';
 const CORE=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./assets/icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
