803ae3d5b723fa3793fb1794fc9249f137adb2e9
eulernasc <129453478+eulernasc@users.noreply.github.com>
2026-08-20 10:17:59 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bruno-gorila-sax-v2';
+const CACHE = 'bruno-gorila-sax-v3';
 const FILES = ['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./assets/icon.svg','./assets/sax-riff.wav'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
